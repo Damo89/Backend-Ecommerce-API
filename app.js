996f7e1df8ebc3c531a7630989431327e1a3044b
Damien Thomas <damien.thomas89@hotmail.co.uk>
2025-05-05 20:48:45 +0100

    Port updated on the backend to avoid conflict with the frontend

--- app.js
+++ app.js
@@ -57,5 +57,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something broke!' });
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- swagger/swagger.js
+++ swagger/swagger.js
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000', //http://localhost:3000/api-docs/
+        url: 'http://localhost:5000', //http://localhost:5000/api-docs/
         description: 'Development server',
       },
     ],
